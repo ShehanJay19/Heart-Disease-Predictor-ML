4f68f3dcd70c6c78247e4766d90e2a36adf0429d
Shehan Jayasinghe <shehanjaya1921@gmail.com>
2025-10-29 21:28:12 +0530

    some css added to this

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
- 
+from flask import Flask,render_template,request
+import joblib
+import numpy as np
+
+model=joblib.load('heart_risk_prediction_regression_model.sav')
+
+app(Flask(__name__))
+
+@app.route('/')
+def index():
+
+    return render_template('index.html')
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    
+    result=request.form
+    
+    name
